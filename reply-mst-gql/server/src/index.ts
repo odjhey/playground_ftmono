8e2ac173e6cbdf3edae738c3be2f81b336e8f167
Odee <odjhey@gmail.com>
2019-12-12 15:57:46 +0800

    update replymee nested query

--- reply-mst-gql/server/src/index.ts
+++ reply-mst-gql/server/src/index.ts
@@ -1,10 +1,31 @@
 import { ApolloServer, gql } from "apollo-server";
+import mongoose from "mongoose";
+import { ObjectID } from "mongodb";
+
+const Schema = mongoose.Schema;
+ObjectID.prototype.valueOf = function() {
+  return this.toString();
+};
+
+const CommentSchema = new Schema(
+  {
+    message: {
+      type: String
+    },
+    replyToID: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+const commentModel = mongoose.model("Comment", CommentSchema);
 
 const typeDefs = gql`
   type Comment {
     id: ID!
     message: String
-    replyToID: String
+    replyTo: Comment
   }
 
   type Query {
@@ -28,10 +49,30 @@ const comments = [
 const resolvers = {
   Query: {
     comments: () => comments
+  },
+  Comment: {
+    replyTo: parent => {
+      const replyToComment = comments.filter(c => parent.replyToID == c.id);
+      return replyToComment.length > 0 ? replyToComment[0] : undefined;
+    }
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+//mongoURI=mongodb://localhost:27017/sample1
+mongoose
+  .connect("mongodb://localhost:27017/repllymee")
+  .then(() => console.log("Mongo on"))
+  .catch(err => console.log(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: session => {
+    return {
+      commentModel
+    };
+  }
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
